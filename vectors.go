97787b2fbb4691651aba7403c053338c0b6dc701
rudsonalves <rudsonalves67@gmail.com>
2022-05-11 15:03:40 -0300

    Small changes

--- vectors.go
+++ vectors.go
@@ -1,38 +1,43 @@
 package mathlib
 
-import "math"
+import (
+	"math"
+)
 
+// Vector define a vector
 type Vector struct {
-	x float64
-	y float64
-	z float64
+	X float64
+	Y float64
+	Z float64
 }
 
+// Module returna a module from vector
 func (v Vector) Module() float64 {
-	return math.Sqrt(math.Pow(v.x, 2) + math.Pow(v.y, 2) + math.Pow(v.z, 2))
+	return math.Sqrt(math.Pow(v.X, 2) + math.Pow(v.Y, 2) + math.Pow(v.Z, 2))
 }
 
+// Add make a addition from vectores a and b
 func (a Vector) Add(b Vector) (r Vector) {
-	r.x = a.x + b.x
-	r.y = a.y + b.y
-	r.z = a.z + b.z
+	r.X = a.X + b.X
+	r.Y = a.Y + b.Y
+	r.Z = a.Z + b.Z
 	return
 }
 
 func (a Vector) Sub(b Vector) (r Vector) {
-	r.x = a.x - b.x
-	r.y = a.y - b.y
-	r.z = a.z - b.z
+	r.X = a.X - b.X
+	r.Y = a.Y - b.Y
+	r.Z = a.Z - b.Z
 	return
 }
 
 func (a Vector) Scalar(b Vector) float64 {
-	return a.x*b.x + a.y*b.y + a.z*b.z
+	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
 }
 
 func (a Vector) Cross(b Vector) (r Vector) {
-	r.x = a.y*b.z - a.z*b.y
-	r.y = a.z*b.x - a.x*b.z
-	r.z = a.x*b.y - a.y*b.z
+	r.X = a.Y*b.Z - a.Z*b.Y
+	r.Y = a.Z*b.X - a.X*b.Z
+	r.Z = a.X*b.Y - a.Y*b.Z
 	return
 }
