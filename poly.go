c4be232dec4831a502d53b24b1d08c520f402744
Rudson R Alves <rudsonalves@yahoo.com.br>
2024-01-18 07:26:36 -0300

    Update poly.go

--- poly.go
+++ poly.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-// SolvePoly2: returns the real roots of the second order polynomial
+// SolvePoly2 returns the real roots of the second order polynomial
 func SolvePoly2(a, b, c float64) (x1, x2 float64, err error) {
 	d := delta2(a, b, c)
 	if d < 0 {
@@ -18,7 +18,7 @@ func SolvePoly2(a, b, c float64) (x1, x2 float64, err error) {
 	return
 }
 
-// delta2: internal function
+// delta2 internal function
 func delta2(a, b, c float64) float64 {
 	return math.Pow(b, 2) - 4*a*c
 }
