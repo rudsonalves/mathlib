f91ff05b8e875ccc8cd17f86f0b6bd08172b7f01
Rudson R Alves <rudsonalves@yahoo.com.br>
2024-01-18 07:26:04 -0300

    Update vectors.go

--- vectors.go
+++ vectors.go
@@ -6,71 +6,71 @@ import (
 	"strings"
 )
 
-// Vector: define a vector
+// Vector define a vector
 type Vector struct {
 	X float64
 	Y float64
 	Z float64
 }
 
-// Module: return a module from vector
+// Module return a module from vector
 func (v Vector) Module() float64 {
 	return math.Sqrt(math.Pow(v.X, 2) + math.Pow(v.Y, 2) + math.Pow(v.Z, 2))
 }
 
-// Add: perform the addition between a and b
-func (a Vector) Add(b Vector) (r Vector) {
-	r.X = a.X + b.X
-	r.Y = a.Y + b.Y
-	r.Z = a.Z + b.Z
+// Add perform the addition between a and b
+func (v Vector) Add(u Vector) (r Vector) {
+	r.X = v.X + u.X
+	r.Y = v.Y + u.Y
+	r.Z = v.Z + u.Z
 	return
 }
 
-// Sub: perform the subtraction between a and b
-func (a Vector) Sub(b Vector) (r Vector) {
-	r.X = a.X - b.X
-	r.Y = a.Y - b.Y
-	r.Z = a.Z - b.Z
+// Sub perform the subtraction between a and b
+func (v Vector) Sub(u Vector) (r Vector) {
+	r.X = v.X - u.X
+	r.Y = v.Y - u.Y
+	r.Z = v.Z - u.Z
 	return
 }
 
-// Scalar: perform the scalar product between a and b
-func (a Vector) Scalar(b Vector) float64 {
-	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
+// Scalar perform the scalar product between a and b
+func (v Vector) Scalar(u Vector) float64 {
+	return v.X*u.X + v.Y*u.Y + v.Z*u.Z
 }
 
-// Cross: perform the cross product between a and b
-func (a Vector) Cross(b Vector) (r Vector) {
-	r.X = a.Y*b.Z - a.Z*b.Y
-	r.Y = a.Z*b.X - a.X*b.Z
-	r.Z = a.X*b.Y - a.Y*b.X
+// Cross perform the cross product between a and b
+func (v Vector) Cross(u Vector) (r Vector) {
+	r.X = v.Y*u.Z - v.Z*u.Y
+	r.Y = v.Z*u.X - v.X*u.Z
+	r.Z = v.X*u.Y - v.Y*u.X
 	return
 }
 
-// String - redefine string() to Vector
-func (r Vector) String() string {
-	str_out := ""
-	if r.X != 0 {
-		str_out += fmt.Sprintf("%si", cleanZeros(r.X))
+// String redefine string() to Vector
+func (v Vector) String() string {
+	strOut := ""
+	if v.X != 0 {
+		strOut += fmt.Sprintf("%si", cleanZeros(v.X))
 	}
-	if r.Y != 0 {
-		if len(str_out) > 0 {
-			str_out += fmt.Sprintf(" + %sj", cleanZeros(r.Y))
+	if v.Y != 0 {
+		if len(strOut) > 0 {
+			strOut += fmt.Sprintf(" + %sj", cleanZeros(v.Y))
 		} else {
-			str_out += fmt.Sprintf("%sj", cleanZeros(r.Y))
+			strOut += fmt.Sprintf("%sj", cleanZeros(v.Y))
 		}
 	}
-	if r.Z != 0 {
-		if len(str_out) > 0 {
-			str_out += fmt.Sprintf(" + %sk", cleanZeros(r.Z))
+	if v.Z != 0 {
+		if len(strOut) > 0 {
+			strOut += fmt.Sprintf(" + %sk", cleanZeros(v.Z))
 		} else {
-			str_out += fmt.Sprintf("%sk", cleanZeros(r.Z))
+			strOut += fmt.Sprintf("%sk", cleanZeros(v.Z))
 		}
 	}
-	if len(str_out) == 0 {
+	if len(strOut) == 0 {
 		return "0"
 	}
-	return str_out
+	return strOut
 }
 
 func cleanZeros(fstr float64) string {
